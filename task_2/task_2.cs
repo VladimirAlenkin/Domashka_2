543461acc5d2a4beb1be1f526a2e49006a2541c8
Vladimir <valenkin@yandex.ru>
2023-01-20 00:46:28 +0500

    task_2

---
+++ task_2/task_2.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Задача 2: по двум заданным числам проверять является ли одно квадратом другого");
+Console.WriteLine("Введите число А");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B");
+int B = int.Parse(Console.ReadLine()!);
+
+if (B == A*A)
+{
+    Console.WriteLine("Число " + B + " - является квадратом числа " + A);
+}
+else if (A == B*B)
+{
+    Console.WriteLine("Число " + A + " - является квадратом числа " + B);
+}
+else{
+    Console.WriteLine("Ни " + A +" , ни " + B + " не являются квадратом друг друга");
+}
