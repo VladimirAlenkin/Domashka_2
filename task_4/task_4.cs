c2e53ef00bf42846a7fba06fbb4519c822c7a8fb
Vladimir <valenkin@yandex.ru>
2023-01-20 00:55:38 +0500

    task_4

---
+++ task_4/task_4.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Задача 4: найти расстояние между точками в пространстве 2D/3D");
+Console.WriteLine("Выберите в какой системе будем искать расстояние (2D/3D). Введите 2 или 3.");
+int N = int.Parse(Console.ReadLine()!);
+if (N == 2)
+{
+    Console.WriteLine("Задайте координату X точки A");
+    int X1 = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Задайте координату Y точки A");
+    int Y1 = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Задайте координату X точки B");
+    int X2 = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Задайте координату Y точки B");
+    int Y2 = int.Parse(Console.ReadLine()!);
+    double result = MathF.Sqrt ((X2 - X1)*(X2 - X1) + (Y2 - Y1)*(Y2 - Y1));
+    Console.Write("Расстояние от точкт А до точки B равно: ");
+    Console.Write(result);
+}
+if (N == 3)
+{
+    Console.WriteLine("Задайте координату X точки A");
+    int X1 = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Задайте координату Y точки A");
+    int Y1 = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Задайте координату Z точки A");
+    int Z1 = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Задайте координату X точки B");
+    int X2 = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Задайте координату Y точки B");
+    int Y2 = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Задайте координату Z точки B");
+    int Z2 = int.Parse(Console.ReadLine()!);
+    double result = MathF.Sqrt ((X2 - X1)*(X2 - X1) + (Y2 - Y1)*(Y2 - Y1) + (Z2 - Z1)*(Z2 - Z1));
+    Console.Write("Расстояние от точкт А до точки B равно: ");
+    Console.Write(result);
+}
+if (N < 2 | N > 3)
+{
+    Console.WriteLine("Вы ввели неверное значение. Попробуйте ещё раз.");
+}
+
