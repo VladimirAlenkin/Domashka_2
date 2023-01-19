af21be44b0316e4ae7d7d732094d48dcfbc1014f
Vladimir <valenkin@yandex.ru>
2023-01-20 00:40:13 +0500

    task_1

---
+++ task_1/task_1.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Задача 1: Дано число обозначающее день недели. Выяснить является номер дня недели выходным");
+Console.WriteLine("Введите число от 1 до 7");
+int day = int.Parse(Console.ReadLine()!);
+if (day > 7 | day < 1) 
+{
+    Console.WriteLine("Введено некорректное число");
+}
+
+else if (day == 6 | day == 7)
+{
+    Console.WriteLine("Этот день - выходной");
+}
+else 
+{
+    Console.WriteLine("Этот день - точно не выходной");
+}
+
